8f1721ef364baab6a49cde18c33ac4b337cc6a53
Vasily Ulianko <vasily.ulianko@gmail.com>
2016-06-02 23:07:14 +0100

    Update to use bootRun locally

--- src/main/resources/public/assets/js/app.js
+++ src/main/resources/public/assets/js/app.js
@@ -1,7 +1,7 @@
 // app.js
 // create our angular app and inject ngAnimate and ui-router
 // =============================================================================
-angular.module('formApp', ['ngAnimate', 'ui.router'])
+angular.module('lextransBookingApp', ['ngAnimate', 'ui.router'])
 
     // configuring our routes
     // =============================================================================
@@ -10,43 +10,43 @@ angular.module('formApp', ['ngAnimate', 'ui.router'])
         $stateProvider
 
         // route to show our basic form (/form)
-            .state('form', {
-                url: '/form',
+            .state('appointment', {
+                url: '/appointment',
                 templateUrl: 'form.html',
-                controller: 'formController'
+                controller: 'appointmentFormController'
             })
 
             // nested states
             // each of these sections will have their own view
             // url will be nested (/form/profile)
-            .state('form.contact', {
+            .state('appointment.contact', {
                 url: '/contact',
                 templateUrl: 'form-contact.html'
             })
 
             // url will be /form/interests
-            .state('form.payment', {
+            .state('appointment.payment', {
                 url: '/payment',
                 templateUrl: 'form-payment.html'
             })
 
             // url will be /form/payment
-            .state('form.confirmation', {
+            .state('appointment.confirmation', {
                 url: '/confirmation',
                 templateUrl: 'form-confirmation.html'
             });
 
         // catch all route
         // send users to the form page
-        $urlRouterProvider.otherwise('/form/contact');
+        $urlRouterProvider.otherwise('/appointment/contact');
     })
 
     // our controller for the form
     // =============================================================================
-    .controller('formController', function($scope) {
+    .controller('appointmentFormController', function($scope) {
 
         // we will store all of our form data in this object
-        $scope.formData = {};
+        $scope.appointmentForm = {};
 
         // function to process the form
         $scope.processForm = function() {
