22d9867c16e69830e1395dd0bdec4dc0095344f4
Vasily Ulianko <vulianko@vsware.ie>
2016-06-09 13:53:16 +0100

    Import initial data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+delete from appointment_city;
+
+INSERT INTO appointment_city (id, name) values (1, 'London');
+INSERT INTO appointment_city (id, name) values (2, 'Dublin');
+INSERT INTO appointment_city (id, name) values (3, 'Cork');
+
